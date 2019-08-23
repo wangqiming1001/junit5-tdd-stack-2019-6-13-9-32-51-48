0d16c28100a4c5c595261db38f9c302e623a9ced
wangqiming1001 <512821390@qq.com>
2019-08-23 18:41:23 +0800

    重构PlayGames类

--- src/main/java/com/thoughtworks/tdd/PlayGames.java
+++ src/main/java/com/thoughtworks/tdd/PlayGames.java
@@ -7,32 +7,25 @@ public class PlayGames {
 	private static String WHIZZ = "WHIZZ";
 	
 	public String fizzBuzzWhizz(int i) {
-		// TODO Auto-generated method stub
+		
+		String result = "";
 		if (i < 1 || i > 120) {
 			return "please input valid number !";
 		}
-		if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
-			return FIZZ + BUZZ + WHIZZ;
-		}
-		if (i % 3 == 0 && i % 5 == 0) {
-			return FIZZ + BUZZ;
-		}
-		if (i % 3 == 0 && i % 7 == 0) {
-			return FIZZ + WHIZZ;
-		}
-		if (i % 5 == 0 && i % 7 == 0) {
-			return BUZZ + WHIZZ;
-		}
 		if (i % 3 == 0) {
-			return FIZZ;
+			result += FIZZ;
 		}
 		if (i % 5 == 0) {
-			return BUZZ;
+			result += BUZZ;
 		}
 		if (i % 7 == 0) {
-			return WHIZZ;
+			result += WHIZZ;
 		}
-		return String.valueOf(i);
+		if (result == "") {
+			result = String.valueOf(i);
+		}
+		System.out.println(result);
+		return result;   
 	}
 
 
